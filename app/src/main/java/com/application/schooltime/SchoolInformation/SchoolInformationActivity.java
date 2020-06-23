aeafe7e92d07e85a555a89ff6b3213e9d83e24ca
ParasRawat <parasmyname@gmail.com>
2020-06-23 17:46:02 +0530

    progress dialog, ui spinner dropdown, shared preferences

--- app/src/main/java/com/application/schooltime/SchoolInformation/SchoolInformationActivity.java
+++ app/src/main/java/com/application/schooltime/SchoolInformation/SchoolInformationActivity.java
@@ -2,6 +2,7 @@ package com.application.schooltime.SchoolInformation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 
 import com.android.volley.Request;
@@ -23,6 +25,7 @@ import com.application.schooltime.R;
 import com.application.schooltime.SchoolActivity;
 import com.application.schooltime.Utilities.Constants;
 import com.application.schooltime.Utilities.PrefManager;
+import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -34,16 +37,17 @@ import java.util.List;
 
 
 public class SchoolInformationActivity extends AppCompatActivity {
-//********** THIS ACTIVITY WILL BE FIRED WHEN THE CHILD SELECTS HIS SCHOOL FOR THE FIRST TIME, OTHER THAN THAT IT WONT BE FIRED.....*********//
+    //********** THIS ACTIVITY WILL BE FIRED WHEN THE CHILD SELECTS HIS SCHOOL FOR THE FIRST TIME, OTHER THAN THAT IT WONT BE FIRED.....*********//
     PrefManager prefManager;
     Spinner schoolSpinner;
     int schoolId;
     String schoolName;
     Button btnSubmit;
-   List<String> schoolNames;
-    String []schoolUrls;
+    List<String> schoolNames;
+    String[] schoolUrls;
     ArrayAdapter<String> schoolAdapter;
-    public static final String TAG="response";
+    public static final String TAG = "response";
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,31 +55,33 @@ public class SchoolInformationActivity extends AppCompatActivity {
 
         prefManager = new PrefManager(this);
 //*********************************ONE TIME CHECK ////////////////////////////
-//        if(prefManager.getSchool()!=null){
-//            launchSchoolAcitivty();
-//            finish();
-//        }
+        if(prefManager.getSchoolUrl()!=null){
+            launchSchoolAcitivty();
+            finish();
+        }
 
 //        if (Build.VERSION.SDK_INT >= 21) {
 //            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 //        }
 
-
-
-
-
-
         setContentView(R.layout.activity_school_information);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please wait while we fetch the schools information");
+        progressDialog.setTitle("Fetching Schools Data");
+        progressDialog.setCancelable(false);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.setMax(100);
+        progressDialog.show();
 
-        schoolSpinner= findViewById(R.id.schools_spinner);
+        schoolSpinner = findViewById(R.id.schools_spinner);
 
 
         //**************************INITIATING API REQUEST USING VOLLEY//
-        RequestQueue queue= Volley.newRequestQueue(this);
+        RequestQueue queue = Volley.newRequestQueue(this);
         String url = "https://raw.githubusercontent.com/MontbitTech/e-EdPort-cms-app/master/cms_list.json";
 
 
-        JsonArrayRequest jsonArrayRequest= new JsonArrayRequest(
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
                 Request.Method.GET,
                 url,
                 null,
@@ -84,21 +90,27 @@ public class SchoolInformationActivity extends AppCompatActivity {
                     public void onResponse(JSONArray response) {
 
                         try {
-                            schoolNames= new ArrayList<>();
+                            schoolNames = new ArrayList<>();
                             schoolUrls = new String[response.length()];
-                            for(int i=0;i<response.length();i++){
+                            for (int i = 0; i < response.length(); i++) {
 
                                 JSONObject school = response.getJSONObject(i);
 
                                 schoolNames.add(school.getString("schoolName"));
-                                schoolUrls[i]= school.getString("schoolUrl");
+                                schoolUrls[i] = school.getString("schoolUrl");
+
+                                int progress = 100/(response.length())*i;
+                                progressDialog.incrementProgressBy(progress);
 
                             }
-                            schoolAdapter=  new ArrayAdapter<String>(getApplicationContext(),R.layout.my_simple_spinner_dropdown, schoolNames);
+
+                            //***** setting up the adapter ************//
+
+                            schoolAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.my_simple_spinner_dropdown, schoolNames);
                             schoolAdapter.setDropDownViewResource(R.layout.my_simple_spinner_item);
                             schoolSpinner.setAdapter(schoolAdapter);
-
-                        }catch (Exception e){
+                            progressDialog.dismiss();
+                        } catch (Exception e) {
                             e.printStackTrace();
                         }
 
@@ -115,21 +127,12 @@ public class SchoolInformationActivity extends AppCompatActivity {
         queue.add(jsonArrayRequest);
 
 
-
-
-
-
-
-
-
-
-        btnSubmit=findViewById(R.id.btnSubmit);
+        btnSubmit = findViewById(R.id.btnSubmit);
 
         schoolSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                schoolId=position;
-                schoolName=(String) parent.getItemAtPosition(position);
+                schoolId = position;
 
 
             }
@@ -140,30 +143,18 @@ public class SchoolInformationActivity extends AppCompatActivity {
             }
         });
 
-//        btnSubmit.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if(schoolId==0){
-//                    // do nothing
-//                    // #todo
-//                }
-//
-//                else if(schoolId==1){
-//                    prefManager.setSchoolFirstTime(schoolName);
-//                    prefManager.setSchoolUrl(Constants.DEMO_SCHOOL_URL);
-//                    startActivity(new Intent(SchoolInformationActivity.this,SchoolActivity.class));
-//                    finish();
-//                }
-//                else{
-//                    prefManager.setSchoolFirstTime(schoolName);
-//                    prefManager.setSchoolUrl(Constants.MAIN_SCHOOL_URL);
-//                    startActivity(new Intent(SchoolInformationActivity.this,SchoolActivity.class));
-//                    finish();
-//                }
-//
-//
-//            }
-//        });
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = schoolUrls[schoolId];
+
+                prefManager.setSchoolUrl(url);
+
+                Snackbar.make(v, url, Snackbar.LENGTH_LONG).show();
+                launchSchoolAcitivty();
+                finish();
+            }
+        });
 
     }
 
